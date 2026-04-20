817c4916a381d3d5c2efb52669b9c7d4a3fdb4c8
Michael Heimpold <michael.heimpold@chargebyte.com>
2026-04-20 14:37:50 +0200

    rtc: rv3028: swap registration of nvmem sub-devices
    At the moment, it is only possible to have exactly one DT sub-node for
    nvmem fixed-layout. However, the driver registers two nvmem devices:
    first for some battery backed bytes, second one for EEPROM area.
    
    This leads to the situation, that the fixed-layout subnode is used for
    both nvmem devices, but we can only define and use a single reference
    to a cell. This means that the reference is pointing to the 'first'
    device, which is wrong in our case.
    
    So as a workaround for now, swap the registration of the two nvmem
    devices until the issues is discussed and sorted out with maintainers.
    
    Signed-off-by: Michael Heimpold <michael.heimpold@chargebyte.com>

--- drivers/rtc/rtc-rv3028.c
+++ drivers/rtc/rtc-rv3028.c
@@ -1018,10 +1018,10 @@ static int rv3028_probe(struct i2c_client *client)
 	if (ret)
 		return ret;
 
-	nvmem_cfg.priv = rv3028->regmap;
-	devm_rtc_nvmem_register(rv3028->rtc, &nvmem_cfg);
 	eeprom_cfg.priv = rv3028;
 	devm_rtc_nvmem_register(rv3028->rtc, &eeprom_cfg);
+	nvmem_cfg.priv = rv3028->regmap;
+	devm_rtc_nvmem_register(rv3028->rtc, &nvmem_cfg);
 
 #ifdef CONFIG_COMMON_CLK
 	rv3028_clkout_register_clk(rv3028, client);
